fc4c60b15ed2050d262b18fec8784b8648f00e9b
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-22 18:42:53 +0300

    fix: nav links focus state

--- components/app/AppNavItem.vue
+++ components/app/AppNavItem.vue
@@ -1,7 +1,8 @@
 <template>
   <NuxtLink
     class="w-12 h-12 flex items-center justify-center rounded-xl transition-all
-  hover:bg-green-400 hover:text-white hover:drop-shadow-md"
+  hover:bg-green-400 hover:text-white hover:drop-shadow-md
+  focus:bg-green-400 focus:text-white focus:drop-shadow-md outline-none"
   activeClass="bg-green-500 hover:bg-green-500 text-white drop-shadow-md"
   >
     <slot/>
