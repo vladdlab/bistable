1c785f3905c3e17f136c392302fb95af664204e4
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-22 18:42:14 +0300

    feat: add charts page

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,14 @@
         <AppNavItem to="/dashboard">
           <BaseIcon name="material-symbols:space-dashboard-rounded" />
         </AppNavItem>
-        <AppNavItem to="/medicines">
-          <BaseIcon name="game-icons:medicines" />
-        </AppNavItem>
         <AppNavItem to="/calendar">
           <BaseIcon name="material-symbols:calendar-month" />
+        </AppNavItem>
+        <AppNavItem to="/charts">
+          <BaseIcon name="ic:round-insert-chart-outlined" />
+        </AppNavItem>
+        <AppNavItem to="/medicines">
+          <BaseIcon name="game-icons:medicines" />
         </AppNavItem>
         <AppNavItem to="/settings">
           <BaseIcon name="material-symbols:settings" />
@@ -20,7 +23,7 @@
     </AppSidebar>
 
     <AppWrapper>
-      <AppHeader/>
+      <AppHeader />
       <AppContent>
         <slot/>
       </AppContent>
