964a4515f9df136d86e2addcb2b1558ff75d7151
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-23 08:31:54 +0300

    fix: app header

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="h-14 bg-white">
-
+  <div class="flex flex-row items-center h-16 bg-white px-5">
+    <BaseInput v-model="search" placeholder="Поиск" />
+    <BaseAvatar class="ml-auto">
+      <BaseIcon name="mdi:user-circle" class="text-green-400" size="lg"/>
+    </BaseAvatar>
   </div>
 </template>
 
 <script setup>
-
+const search = ref('')
 </script>
 
 <style lang="scss" scoped>
