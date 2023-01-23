d347d8b4038d06f2548c707023c967fce9e61098
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-23 08:31:37 +0300

    feat: base icon

--- components/base/BaseIcon.vue
+++ components/base/BaseIcon.vue
@@ -1,9 +1,18 @@
 <template>
-  <Icon :name="name" class="w-8 h-8" />
+  <Icon :name="name" :class="size" />
 </template>
 
-<script setup>
-defineProps({
-  name: String
+<script setup lang="ts">
+import { computed } from 'vue';
+const props = withDefaults(defineProps<{
+  name: string,
+  size?: 'sm' | 'md' | 'lg'
+}>(), {
+  size: 'md'
+})
+
+const size = computed(() => {
+  const sizes = { 'sm': 4, 'md': 8, 'lg': 10 }
+  return `w-${sizes[props.size]} h-${sizes[props.size]}`
 })
 </script>
