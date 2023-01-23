10a6ef8d5b491941b9970356192336146934fe26
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-23 08:31:29 +0300

    feat: base input

---
+++ components/base/BaseInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+    type="text"
+    v-bind="$attrs"
+    :value="modelValue"
+    @input="handleInput"
+    class="py-1.5 px-3 outline-green-500 outline-offset-2 border-gray-200 bg-slate-100 rounded-lg"
+    >
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+const emit = defineEmits(['update:modelValue'])
+const props =  defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+function handleInput(event: Event) {
+  emit('update:modelValue', (event.target as HTMLInputElement).value)
+}
+</script>
