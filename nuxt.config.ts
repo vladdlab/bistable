79fa89866f5d1218b6b77339d5233d4347aaf63b
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-01 15:32:07 +0300

    feat: layout transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
     },
   },
   app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' },
     pageTransition: { name: 'page', mode: 'out-in' }
   },
   runtimeConfig: {
