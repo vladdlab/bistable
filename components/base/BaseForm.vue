16267bdc0def4efd5e48a42f5d27839c48af24d1
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-01 15:33:57 +0300

    feat: baseForm

---
+++ components/base/BaseForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="flex flex-col">
+    <slot/>
+
+    <slot name="erros-list">
+      <div class="mt-2 p-2 text-center">
+        <BaseInvalidFeedback v-for="error in errors" :error-message="error" />
+      </div>
+    </slot>
+  </form>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  errors?: string[]
+}>()
+</script>
