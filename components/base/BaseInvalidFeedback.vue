a618c53922dd96b09d094c3d48d5fba68d5bba9c
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-01 15:34:38 +0300

    feat: add base invalid

---
+++ components/base/BaseInvalidFeedback.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-xs text-red-500 mb-2">
+    <slot>
+      {{ errorMessage }}
+    </slot>
+  </p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  errorMessage?: string
+}>()
+</script>
