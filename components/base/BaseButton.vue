405ef1f0d5849f372d8833447d7387cff049c7f4
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-01 15:32:57 +0300

    feat: create BaseButton

---
+++ components/base/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="bg-slate-300 text-slate-700 rounded-md"
+    :class="btnClasses"
+  >
+    <slot v-if="!loading"/>
+    <BaseSpinner v-else/>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  // Styles
+  size?: 'sm' | 'md' | 'lg',
+  loading?: boolean
+}>(), {
+  size: 'md',
+  loading: false
+})
+
+
+// Styles
+const btnClasses = computed(() => {
+  const sizes = {
+    sm: 'px-2 py-0.5',
+    md: 'px-5 py-1',
+    lg: 'px-7 py-3'
+  }
+  return [sizes[props.size]]
+})
+</script>
