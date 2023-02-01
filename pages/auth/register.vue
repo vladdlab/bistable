911a7b529e72a1766215991cc286865c07347d22
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-02-01 15:36:29 +0300

    fix(client): refactor register page

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,68 +1,94 @@
 <template>
   <div class="w-100 h-screen grid grid-cols-12">
-    <div class="flex items-center justify-center col-span-4 p-4">
-      <form @submit.prevent="register" class="flex flex-col items-center gap-3">
-        <BaseInput v-model="name" placeholder="Имя" autocomplete="name" />
-        <span class="text-xs text-red-500">{{ errors.name }}</span>
+    <div class="flex flex-col col-span-5 p-4">
+      <div class="flex-grow flex flex-col items-center justify-center">
+        <BaseForm @submit="register" :errors="registerErrors" class="w-3/5">
+          <h1 class="sr-only">Sign Up Form</h1>
+          <p class="text-4xl font-bold mb-3">Get Started <span>🤝</span></p>
+          <p class="text-slate-500 text-sm mb-8">Let's create your account. We'd love for you to join. Keep in mind, this application is completely free!</p>
 
-        <BaseInput v-model="email" placeholder="E-mail" type="email" autocomplete="email" />
-        <span class="text-xs text-red-500">{{ errors.email }}</span>
+          <BaseInput
+            v-model="name"
+            type="text"
+            label="Name"
+            autocomplete="name"
+            placeholder="What shall we call you?"
+            :error-message="errors.name"
+            size="lg"
+          />
 
-        <BaseInput v-model="password" type="password" placeholder="Пароль" autocomplete="new-password" />
-        <span class="text-xs text-red-500">{{ errors.password }}</span>
+          <BaseInput
+            v-model="email"
+            type="email"
+            label="Email Address"
+            autocomplete="email"
+            placeholder="awesome@email.com"
+            :errorMessage="errors.email"
+            size="lg"
+          />
 
-        <button class="bg-slate-300 text-slate-700 px-5 py-1 rounded-md">Зарегистрироваться</button>
+          <BaseInput
+            v-model="password"
+            label="Password"
+            placeholder="8+ characters required"
+            type="password"
+            autocomplete="new-password"
+            :errorMessage="errors.password"
+            size="lg"
+          />
 
-        <p class="text-xs text-red-500">{{ errorMessage }}</p>
+          <BaseButton class="bg-green-500 text-white mt-5" type="submit" size="lg" :loading="isSubmitting" :disabled="isSubmitting">Sign Up</BaseButton>
+        </BaseForm>
+      </div>
 
-        <NuxtLink to="login" class="text-green-500">У меня уже есть аккаунт</NuxtLink>
-      </form>
+      <p class="text-center">
+        Already have an account?
+        <NuxtLink to="login" class="text-green-500">Log in!</NuxtLink>
+      </p>
     </div>
-    <div class="col-span-8 bg-gradient-to-r from-cyan-500 to-blue-500">
-
-    </div>
+    <div class="col-span-7 bg-rainbow-image bg-center bg-local bg-contain bg-no-repeat"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { useField, useForm } from 'vee-validate';
-import { toFormValidator } from '@vee-validate/zod';
-import { registerUserSchema } from '~~/server/schemas/user.schema';
-const router = useRouter()
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { useField, useForm } from 'vee-validate'
+import { toFormValidator } from '@vee-validate/zod'
+import { registerUserSchema } from '~~/server/schemas/user.schema'
 
 definePageMeta({
-  layout: "auth",
-});
-
+  layout: 'auth',
+  title: 'Sign Up'
+})
 
 // Form Schema
 const validationSchema = toFormValidator(registerUserSchema.shape.body)
-const { handleSubmit, errors } = useForm({
+const { handleSubmit, errors, isSubmitting } = useForm({
   validationSchema,
   initialValues: {
     name: '',
     email: '',
-    password: ''
+    password: '',
   },
-});
+})
 
 // Form fields
-const { value: name } = useField<string>('name');
-const { value: email } = useField<string>('email');
-const { value: password } = useField<string>('password');
+const { value: name } = useField<string>('name')
+const { value: email } = useField<string>('email')
+const { value: password } = useField<string>('password')
 
 // Form's general error text
-const errorMessage = ref('')
+const registerErrors: Ref<string[]> = ref([])
 
 // Form handler
 const register = handleSubmit(async (values) => {
   const { error } = await useFetch('/api/auth/register', { method: 'POST', body: values })
   if (error.value) {
-    errorMessage.value = error.value.message || '';
+    registerErrors.value = [error.value.data.message]
   } else {
-    const router = useRouter();
-    router.push('/dashboard');
+    const router = useRouter()
+    router.push('/dashboard')
   }
 })
 </script>
